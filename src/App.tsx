6fe656ad7f9dfbc4e648d18e552d76508ff0fc8c
Italo Gama <italoflorencio@gmail.com>
2022-02-08 00:19:46 -0300

    Primeiro de muitos

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Room } from './pages/Room';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
